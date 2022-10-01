4b22a2082a3bbb278ab4675890306f0623d68190
hneumann <18195232+hneumann@users.noreply.github.com>
2022-10-01 17:33:41 +0200

    Add all the monks

---
+++ LudumDare/Assets/Scripts/MonkController.cs
@@ -0,0 +1,36 @@
+
+using System;
+using UnityEngine;
+
+public class MonkController : MonoBehaviour
+{
+    [SerializeField] private GameObject idleMonks;
+    [SerializeField] private GameObject inMonks;
+    [SerializeField] private GameObject outMonks;
+
+    void Start()
+    {
+        outMonks.SetActive(false);
+        idleMonks.SetActive(false);
+        inMonks.SetActive(true);
+    }
+
+    private void Update()
+    {
+        var ones = Mathf.RoundToInt(Time.time) % 10;
+
+        if (ones % 5 is 0 or 5)
+        {
+            idleMonks.SetActive(true);
+            outMonks.SetActive(false);
+            inMonks.SetActive(false);
+            return;
+        }
+
+        idleMonks.SetActive(false);
+        inMonks.SetActive(ones < 5);
+        outMonks.SetActive(ones > 5);
+        
+    }
+    //TODO switch monks correctly
+}
