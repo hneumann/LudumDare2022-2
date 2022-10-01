272bda1963a36463658b16d6d5df8167b45c249c
Robin Schmitz <schmitz.robin@gmx.de>
2022-10-01 18:15:35 +0200

    Experimental Controls 2.0 mit links und rechts

--- LudumDare/Assets/Scripts/BumblebeeController.cs
+++ LudumDare/Assets/Scripts/BumblebeeController.cs
@@ -10,7 +10,6 @@ public class BumblebeeController : MonoBehaviour
     [SerializeField] private AnimationCurve forceFactorCurve;
     [SerializeField] private float maxVerticalVelocity;
 
-
     void Update()
     {
         //var verticalValue = Input.GetAxis("Vertical");
@@ -20,23 +19,37 @@ public class BumblebeeController : MonoBehaviour
         } 
         if(Input.GetKeyDown(KeyCode.DownArrow)||Input.GetKeyDown(KeyCode.S)) {
             verticalValue = -1f;
+        }
+
+        var horizontalValue = 0f;
+        if(Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.D)) {
+            horizontalValue = 1f;
         } 
+        if(Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.A)) {
+            horizontalValue = -1f;
+        }
 
-        var verticalForce = 0f;
+        float horizontalForce;
+        float verticalForce;
 
-        if (useForceCurve)
-        {
+        if (useForceCurve) {
             verticalForce = forceFactorCurve.Evaluate(verticalValue);
-        }
-        else
-        {
+            horizontalForce = forceFactorCurve.Evaluate(horizontalValue);
+        } else {
             verticalForce = verticalValue * forceFactor;
+            horizontalForce = horizontalValue * forceFactor;
+            if (verticalForce != 0 || horizontalForce != 0) {
+                Debug.Log("vertical: " + verticalForce + " horizontal: " + horizontalForce);
+            }
+
         }
 
-        var force = Vector2.up * verticalForce;
-        Debug.Log("force: " + force);
+
+        var force = Vector2.up * verticalForce + Vector2.right * horizontalForce;
+        //Debug.Log("force: " + force);
         rigidbody.AddForce(force, ForceMode2D.Impulse);
         
-        rigidbody.velocity = Vector2.up * Mathf.Clamp(rigidbody.velocity.y, -maxVerticalVelocity, maxVerticalVelocity);
+        rigidbody.velocity = Vector2.up * Mathf.Clamp(rigidbody.velocity.y, -maxVerticalVelocity, maxVerticalVelocity) + Vector2.right * Mathf.Clamp(rigidbody.velocity.x, -maxVerticalVelocity, maxVerticalVelocity);
+        //rigidbody.velocity = Vector2.right * Mathf.Clamp(rigidbody.velocity.x, -maxVerticalVelocity, maxVerticalVelocity);
     }
 }
